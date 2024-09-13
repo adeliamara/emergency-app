323a06a16381f1b9c766870095e15768bb86d64b
Daliego <daliego82@gmail.com>
2024-09-13 07:40:03 -0300

    feat: login and register page with the fireauth library

--- lib/modules/auth/pages/login_page.dart
+++ lib/modules/auth/pages/login_page.dart
@@ -1,34 +1,154 @@
+import 'package:emergency_app/components/button.dart';
+import 'package:emergency_app/components/input.dart';
+import 'package:emergency_app/modules/auth/pages/register_page.dart';
+import 'package:emergency_app/modules/emergency_button/emergency_button_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> handleLogin(BuildContext context) async {
+    String username = _emailController.text;
+    String password = _passwordController.text;
+
+    if (username.isEmpty || password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Please enter both username and password'),
+        ),
+      );
+
+      return;
+    }
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Login concluído'),
+        ),
+      );
+
+      _emailController.clear();
+      _passwordController.clear();
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => EmergencyButtonPage()),
+      );
+    } catch (e) {
+      if (e is FirebaseAuthException) {
+        if (e.code == 'user-not-found') {
+          // Show a specific message for "user-not-found"
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('No user found for that email.'),
+            ),
+          );
+        } else if (e.code == 'wrong-password') {
+          // Show a specific message for "wrong-password"
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Wrong password provided for that user.'),
+            ),
+          );
+        } else {
+          // Handle other types of errors
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Erro nas suas credenciais'),
+            ),
+          );
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Erro nas suas credenciais'),
+          ),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text('Login Page'),
-          ElevatedButton(
-            onPressed: () {
-              context.go('/auth/register');
-            },
-            child: Text('Navigate to register'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              context.go('/auth/recovery-password');
-            },
-            child: Text('Navigate to recovery password'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              context.go('/call');
-            },
-            child: Text('Navigate to call'),
-          )
-        ],
+      // appBar: AppBar(
+      //   title: Text('Login'),
+      //   centerTitle: true,
+      // ),
+      backgroundColor: Color(0xffEFF2F9),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Column(children: [
+              Image.asset('assets/images/sirene.png'),
+              Text(
+                "Emergenciers",
+                style: TextStyle(color: Color(0xffff3b30), fontSize: 35),
+              )
+            ]),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Input(
+                  hintText: "Email",
+                  controller: _emailController,
+                  borderRadius: 25.0,
+                  fillColor: Color(0xffF9FBFF),
+                  prefixIcon: Icons.email,
+                  labelText: 'Email',
+                  textColor: Color(0xffBABDC8),
+                ),
+                SizedBox(height: 16.0),
+                Input(
+                  obscureText: true,
+                  controller: _passwordController,
+                  labelText: 'Senha',
+                  hintText: 'Senha',
+                  borderRadius: 25,
+                  prefixIcon: Icons.lock,
+                ),
+                // GestureDetector(
+                //   child:
+                // Text(
+                //   "Esqueci minha senha",
+                //   style: TextStyle(color: Color(0xffff3b30)),
+
+                // ),
+                //   onTap: hadleTab,
+                // )
+                SizedBox(height: 22.0),
+                Button(
+                  text: 'ENTRAR',
+                  onPressed: () {
+                    handleLogin(context);
+                  },
+                ),
+                SizedBox(height: 16.0),
+                Button(
+                  text: 'CRIAR CONTA',
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => RegisterPage()),
+                    );
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 16.0),
+          ],
+        ),
       ),
     );
   }
